96aaaa5b3177b3b90bc16ed7e9adf41a864a1630
seinecle <1244100+seinecle@users.noreply.github.com>
2023-07-24 18:34:45 +0200

    finalized text extraction from pdf region

--- import_pdf_return_png/src/main/java/net/clementlevallois/importers/import_pdf/controller/PdfToPngConverter.java
+++ import_pdf_return_png/src/main/java/net/clementlevallois/importers/import_pdf/controller/PdfToPngConverter.java
@@ -1,5 +1,6 @@
 package net.clementlevallois.importers.import_pdf.controller;
 
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -22,10 +23,26 @@ public class PdfToPngConverter {
         BufferedImage bi;
         for (int page = 0; page < document.getNumberOfPages(); ++page) {
             bi = pr.renderImageWithDPI(page, 100);
+            int width = bi.getWidth();
+            int height = bi.getHeight();
+            int maxWidth = 800;
+            int maxHeight = 800;
+            float factorWidthTooBig = width / maxWidth;
+            float factorHeightTooBig = height / maxHeight;
+            if (factorWidthTooBig > 2 || factorHeightTooBig > 2) {
+                float resizeFactor = Math.max(factorWidthTooBig, factorHeightTooBig);
+                int targetWidth = (int) ((float) width / resizeFactor);
+                int targetHeight = (int) ((float) height / resizeFactor);
+                Image resultingImage = bi.getScaledInstance(targetWidth, targetHeight, Image.SCALE_DEFAULT);
+                BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+                outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+                bi = outputImage;
+            }
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(bi, "PNG", baos);
             results[page] = baos.toByteArray();
         }
+        document.close();
         return results;
     }
 }
